599031c06072af97832c8938814f91737b4f31e6
Kaeden Young <kamayoun@ucsc.edu>
2022-04-25 15:15:42 -0700

    Added Sky background

--- src/prefabs/Enemy_Tank.js
+++ src/prefabs/Enemy_Tank.js
@@ -1,5 +1,5 @@
-// Spaceship prefab
-class Spaceship extends Phaser.GameObjects.Sprite {
+// Enemy Tank prefab
+class Enemy_Tank extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
