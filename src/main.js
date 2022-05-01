4d329182e64a72431f582091dc5e342cd961d003
Kaeden Y <kamayoun@ucsc.edu>
2022-04-30 22:05:41 -0700

    adding comments and points

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/* Kaeden Young
+Rocket Patrol Mod
+Time for completion: 15ish hours
+*/ 
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -13,3 +19,16 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT, keyD;
+
+
+/* Points Breakdown
+Added Copyright-free background music to the play scene (5)
+Create a new scrolling tile sprite for background (5)
+Implement Parallax scrolling (10)
+Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+Create new artwork for all of the in-game assests (rocket, spaceshhips, explosion) (20)
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+Create and implemtn a new weapon (w/ new behavior and graphics) (20)
+Redesign the game's artwork, UI, and sound to change its theme/aestheic (to something other than sci-fi) (60)
+But its really late, so deduct what you think is necessary
+*/
